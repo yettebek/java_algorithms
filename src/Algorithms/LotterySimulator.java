3a07b6e63ce281355edf5058a0084a625603763f
yettebek <yersussmith@gmail.com>
2023-06-10 01:34:38 +0200

    new release updated

--- src/Algorithms/LotterySimulator.java
+++ src/Algorithms/LotterySimulator.java
@@ -7,21 +7,22 @@ public class LotterySimulator {
     public static Random random = new Random();
 
     public static List<Integer> lotteryNumbers() {
-        //Variables
+        // Variables
         List<Integer> cpuNumbers = new ArrayList<Integer>();
-        //Generate 6 positions for the lottery numbers inside the for loop
+        // Generate 6 positions for the lottery numbers inside the for loop
         for (int i = 1; i < 7; i++) {
             while (true) {
-                //Generate the random numbers between 1 and 49
-                int num = random.nextInt(49) + 1; // (49) = (from 0 to 48) + 1 
-                //If the number is not in the list, add it, otherwise, continue generating numbers until it is not in the list (while loop)
-                if (!cpuNumbers.contains(num)) { //true
+                // Generate the random numbers between 1 and 49
+                int num = random.nextInt(49) + 1; // (49) = (from 0 to 48) + 1
+                // If the number is not in the list, add it, otherwise, continue generating
+                // numbers until it is not in the list (while loop)
+                if (!cpuNumbers.contains(num)) { // true
                     cpuNumbers.add(num);
                     break;
                 }
             }
         }
-        //sort the numbers
+        // sort the numbers into ascending order
         Collections.sort(cpuNumbers);
         return cpuNumbers;
     }
@@ -36,25 +37,36 @@ public class LotterySimulator {
     }
 
     public static void main(String[] args) {
-        //Instantiating the lists for the lottery numbers and the complement in the main method
+        // Instantiating the lists for the lottery numbers and the complement in the
+        // main method
         out = lotteryNumbers();
         out2 = lotteryComplement();
+        while (true) {
+            try {
+                // Create a scanner to read the number of combinations to generate
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the number of combinations to generate (1 to 8): ");
+                int combinations = scanner.nextInt();
+                double price = Double.valueOf(combinations);
 
-        //Create a scanner to read the number of combinations to generate
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of combinations to generate (1 to 8): ");
-        int combinations = scanner.nextInt();
+                if (combinations > 0 && combinations < 9) {
 
-        if (combinations > 0 && combinations < 9) {
-            for (int i = 1; i <= combinations; i++) {
-                out = lotteryNumbers();
-                out2 = lotteryComplement();
-                System.out.println(i + ". Lottery numbers: " + out + " - " + out2);
+                    for (int i = 1; i <= combinations; i++) {
+                        out = lotteryNumbers();
+                        out2 = lotteryComplement();
+                        System.out.println(i + ". Lottery numbers: " + out + " - " + out2);
+                    }
+                    System.out.printf("\nTOTAL: %.2f EUR", price * 0.5);
+
+                    break;
+
+                } else {
+                    System.err.println("Invalid number of combinations".toUpperCase());
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("Please. Enter a number between 1 and 8");
             }
-        } else {
-            System.err.println("Invalid number of combinations".toUpperCase());
         }
-
-        scanner.close();
+        // scanner.close();
     }
 }
---
+++ src/SoloLearn/WhoLikesIt.java
@@ -0,0 +1,28 @@
+package SoloLearn;
+
+public class WhoLikesIt {
+
+    public static String namesLikes(String[] names) { //(String ... names)
+        int namesLength = names.length;
+
+        // switch case in case there isn't anybody, when there is 1, when there is 2, when there is 3 and when there is more than 3
+
+        switch (namesLength) {
+            case 0:
+                return String.format("no one likes this");
+            case 1:
+                return String.format("%s likes this", names[0]);
+            case 2:
+                return String.format("%s and %s like this", names[0], names[1]);
+            case 3:
+                return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default:
+                return String.format("%s, %s and %d others like this", names[0], names[1], namesLength - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] newNames = {"Joe","Bob"};
+        System.out.println(namesLikes(newNames));
+    }
+}
