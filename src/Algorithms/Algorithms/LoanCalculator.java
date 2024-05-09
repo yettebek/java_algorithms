87bf7c65274bfe6358876c8367a7926363137247
Ye . <llersussmith@gmail.com>
2024-05-09 23:36:29 +0200

    LoanCalculator.java edited

--- src/Algorithms/Algorithms/LoanCalculator.java
+++ src/Algorithms/Algorithms/LoanCalculator.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 /*Loan Calculator
 
-You take a loan from a friend and need to calculate how much you will owe him after 3 months.
+You take a loan from a friend and need to calculate how much you will owe him after x months.
 You are going to pay him back 10% of the remaining loan amount each month.
-Create a program that takes the loan amount as input, calculates and outputs the remaining amount after 3 months.
+Create a program that takes the loan amount as input, calculates and outputs the remaining amount after x months.
 
 Sample Input: 20000   |  Sample Output: 14580
 
@@ -20,20 +20,30 @@ Remaining amount: 16200
 Month 3:
 Payment: 10% of 16200 = 1620
 Remaining amount: 14580
+Month X:
 */
 public class LoanCalculator {
 
     public static void main(String[] args) {
-        //Variables
+        // Variables
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the total amount ($): ");
-        int amount = scanner.nextInt();
+        double amount = scanner.nextInt();
+        int months = 0;
 
-        //Iterate throuh 3 monthly payments
-        for (int i = 1; i <= 3; i++) {
+        // Iterate till the remaining amount is greater than or equal to 1
+        while (true) {
             amount *= 0.9;
+            double roundedAmount = Math.round(amount * 100.0) / 100.0;
+            if (amount >= 1) {
+                System.out.println("Remaining amount (minus 10%): " + roundedAmount);
+                months++;
+            }else{
+                break;
+            }
         }
-        System.out.println("Remaining amount (after 3 months): " + amount);
+
+        System.out.println("It took " + months + " months to pay off the loan.");
         scanner.close();
     }
 }
